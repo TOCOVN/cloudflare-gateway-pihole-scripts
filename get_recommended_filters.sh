10f6bd24b858b88847e38c0dd06423f59cbf9848
TOCOVN <143512842+TOCOVN@users.noreply.github.com>
2023-09-05 18:57:50 +0700

    Update get_recommended_filters.sh

--- get_recommended_filters.sh
+++ get_recommended_filters.sh
@@ -7,8 +7,7 @@ touch input.csv
 urls=(
   https://raw.githubusercontent.com/bigdargon/hostsVN/master/option/hosts-VN
   https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
-  https://raw.githubusercontent.com/luxysiv/hosts/main/hosts.txt
-  https://raw.githubusercontent.com/Yhonay/antipopads/master/hosts
+  https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt
   https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt
   https://raw.githubusercontent.com/mullvad/dns-blocklists/main/output/doh/doh_adblock.txt
   https://raw.githubusercontent.com/mullvad/dns-blocklists/main/output/doh/doh_gambling.txt
